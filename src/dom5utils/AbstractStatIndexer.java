7b320a10b5d9897d5c602b0c25a2b1661b350a5f
Larz <larzsp@gmail.com>
2017-12-20 16:36:51 -0500

    Refactor unit stat indexer.

--- src/dom5utils/AbstractStatIndexer.java
+++ src/dom5utils/AbstractStatIndexer.java
@@ -22,42 +22,14 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public abstract class AbstractStatIndexer {
-	public static String EXE_NAME = "Dominions5.exe";
-	
-	private static Map<String, boolean[]> largeBitmapCache = new HashMap<String, boolean[]>();
-
-	protected static class CacheKey {
-		String key;
-		int id;
-		public CacheKey(String key, int id) {
-			super();
-			this.key = key;
-			this.id = id;
-		}
-		
-	}
-	protected static Map<CacheKey, String> attrCache = new HashMap<CacheKey, String>();
+	protected static String EXE_NAME = "Dominions5.exe";
 	
 	private static int MASK[] = {0x0001, 0x0002, 0x0004, 0x0008, 0x0010, 0x0020, 0x0040, 0x0080,
 			0x0100, 0x0200, 0x0400, 0x0800, 0x1000, 0x2000, 0x4000, 0x8000};
 
-	protected static XSSFWorkbook readFile(String filename) throws IOException {
-		return new XSSFWorkbook(new FileInputStream(filename));
-	}
-	
 	protected static short getBytes1(long skip) throws IOException {
 		FileInputStream stream = new FileInputStream(EXE_NAME);			
 		short value = 0;
@@ -147,39 +119,6 @@ public abstract class AbstractStatIndexer {
 		return name.toString();
 	}
 	
-	protected static void putBytes2(XSSFSheet sheet, int skip, int column, long start, long size, int count, Callback callback) throws IOException {
-		FileInputStream stream = new FileInputStream(EXE_NAME);			
-		stream.skip(start);
-		int rowNumber = 1;
-		int i = 0;
-		byte[] c = new byte[2];
-		stream.skip(skip);
-		while ((stream.read(c, 0, 2)) != -1) {
-			XSSFRow row = sheet.getRow(rowNumber);
-			rowNumber++;
-			XSSFCell cell = row.getCell(column, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-			String high = String.format("%02X", c[1]);
-			String low = String.format("%02X", c[0]);
-			int value = Integer.decode("0X" + high + low);
-			if (value > 60000) {
-				value = new BigInteger("FFFF" + high + low, 16).intValue();
-			}
-
-			if (callback != null) {
-				cell.setCellValue(callback.found(Integer.toString(value)));
-			} else {
-				cell.setCellValue(value);
-			}
-
-			stream.skip(size-2);
-			i++;
-			if (i >= count) {
-				break;
-			}
-		}
-		stream.close();
-	}
-	
 	protected static List<AttributeValue> getAttributes(long skip, long attrGap) throws IOException {
 		return getAttributes(skip, attrGap, 4l);
 	}
@@ -223,202 +162,6 @@ public abstract class AbstractStatIndexer {
 		return newList;
 	}
 	
-	protected static void putMultipleAttributes(XSSFSheet sheet, String attr, long attrStart, long attrGap, int column, long start, long size, int count, Callback callback) throws IOException {
-		FileInputStream stream = new FileInputStream(EXE_NAME);			
-		stream.skip(start);
-		int i = 0;
-		int k = 0;
-		Set<Integer> posSet = new HashSet<Integer>();
-		long numFound = 0;
-		byte[] c = new byte[2];
-		stream.skip(attrStart);
-		while ((stream.read(c, 0, 2)) != -1) {
-			String high = String.format("%02X", c[1]);
-			String low = String.format("%02X", c[0]);
-			int attribute = Integer.decode("0X" + high + low);
-			if (attribute == 0) {
-				stream.skip(attrGap - numFound*2l);
-				int numColumns = 0;
-				// Values
-				for (int x = 0; x < numFound; x++) {
-					byte[] d = new byte[4];
-					stream.read(d, 0, 4);
-					String high1 = String.format("%02X", d[3]);
-					String low1 = String.format("%02X", d[2]);
-					high = String.format("%02X", d[1]);
-					low = String.format("%02X", d[0]);
-					if (posSet.contains(x)) {
-						int value = new BigInteger(high1 + low1 + high + low, 16).intValue();
-						XSSFRow row = sheet.getRow(i+1);
-						XSSFCell cell = row.getCell(column+numColumns, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);	
-						if (callback == null) {
-							cell.setCellValue(value);
-						} else {
-							if (callback.found(Integer.toString(value)) != null) {
-								cell.setCellValue(callback.found(Integer.toString(value)));
-							}
-						}
-						numColumns++;
-					}
-				}
-
-				stream.skip(size - 2 - attrGap - numFound*4l);
-				numFound = 0;
-				posSet.clear();
-				k = 0;
-				i++;
-			} else {
-				if (attr.indexOf(low + high) != -1) {
-					posSet.add(k);
-				}
-				k++;
-				numFound++;
-			}				
-			if (i >= count) {
-				break;
-			}
-		}
-		stream.close();
-	}
-
-	protected static void putAttribute(XSSFSheet sheet, String attr, long attrStart, long attrGap, int column, long start, long size, int count, boolean append, Callback callback) throws IOException {
-        FileInputStream stream = new FileInputStream(EXE_NAME);			
-		stream.skip(start);
-		int rowNumber = 1;
-		int i = 0;
-		int k = 0;
-		int pos = -1;
-		long numFound = 0;
-		byte[] c = new byte[2];
-		stream.skip(attrStart);
-		while ((stream.read(c, 0, 2)) != -1) {
-			String high = String.format("%02X", c[1]);
-			String low = String.format("%02X", c[0]);
-			int attribute = Integer.decode("0X" + high + low);
-			if (attribute == 0) {
-				boolean found = false;
-				int value = 0;
-				stream.skip(attrGap - numFound*2l);
-				
-				// Values
-				for (int x = 0; x < numFound; x++) {
-					byte[] d = new byte[4];
-					stream.read(d, 0, 4);
-					String high1 = String.format("%02X", d[3]);
-					String low1 = String.format("%02X", d[2]);
-					high = String.format("%02X", d[1]);
-					low = String.format("%02X", d[0]);
-					if (x == pos) {
-						value = new BigInteger(high1 + low1 + high + low, 16).intValue();
-						found = true;
-					}
-				}
-				
-				XSSFRow row = sheet.getRow(rowNumber);
-				rowNumber++;
-				
-				XSSFCell cell = row.getCell(column, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-				if (found) {
-					if (callback == null) {
-						if (append) {
-							String origVal = cell.getStringCellValue();
-							cell.setCellValue(origVal + value);
-						} else {
-							cell.setCellValue(value);
-						}
-					} else {
-						if (append) {
-							String origVal = cell.getStringCellValue();
-							if (callback.found(Integer.toString(value)) != null) {
-								cell.setCellValue(origVal + callback.found(Integer.toString(value)));
-							}
-						} else {
-							if (callback.found(Integer.toString(value)) != null) {
-								cell.setCellValue(callback.found(Integer.toString(value)));
-							}
-						}
-					}
-				} else {
-					if (callback == null) {
-						cell.setCellValue("");
-					} else {
-						if (callback.notFound() != null) {
-							cell.setCellValue(callback.notFound());
-						}
-					}
-				}
-				stream.skip(size - 2 - attrGap - numFound*4l);
-				numFound = 0;
-				pos = -1;
-				k = 0;
-				i++;
-			} else {
-				if (attr.indexOf(low + high) != -1) {
-					if (pos == -1) {
-						pos = k;
-					}
-				}
-				k++;
-				numFound++;
-			}				
-			if (i >= count) {
-				break;
-			}
-		}
-		stream.close();
-	}
-
-	protected static boolean[] largeBitmap(String fieldName, long bitmapOffset, long start, long size, int count, String[][] values) throws IOException {
-		return largeBitmap(fieldName, bitmapOffset, start, size, count, values, false);
-	}
-	
-	protected static boolean[] largeBitmap(String fieldName, long bitmapOffset, long start, long size, int count, String[][] values, boolean debug) throws IOException {
-		boolean[] boolArray = largeBitmapCache.get(fieldName);
-		if (boolArray != null) {
-			return boolArray;
-		}
-		FileInputStream stream = new FileInputStream(EXE_NAME);			
-		stream.skip(start);
-		int i = 0;
-		byte[] c = new byte[2];
-		boolArray = new boolean[count];
-		stream.skip(bitmapOffset);
-		while ((stream.read(c, 0, 2)) != -1) {
-			boolean found = false;
-			if (debug) { System.out.print("(" + (i+1) + ") "); }
-			for (int k = 0; k < values.length; k++) {
-				String high = String.format("%02X", c[1]);
-				String low = String.format("%02X", c[0]);
-				int val = Integer.decode("0X" + high + low);
-				if (val > 0) {
-					if (debug) { System.out.print((k+1)+":{"); }
-					for (int j=0; j < 16; j++) {
-						if ((val & MASK[j]) != 0) {
-							if (debug) { System.out.print((found?",":"") + (values[k][j].equals("")?("*****"+(j+1)+"*****"):values[k][j])); }
-							if (values[k][j].equals(fieldName)) {
-								found = true;
-							}
-						}
-					}
-					if (debug) { System.out.print("}"); }
-				}
-				if (k < values.length-1) {
-					stream.read(c, 0, 2);
-				}
-			}
-			boolArray[i] = found;
-			if (debug) { System.out.println(" "); }
-			stream.skip(bitmapOffset);
-			i++;
-			if (i >= count) {
-				break;
-			}
-		}
-		stream.close();
-		largeBitmapCache.put(fieldName, boolArray);
-		return boolArray;
-	}
-
 	protected static List<String> largeBitmap(long start, String[][] values) throws IOException {
 		FileInputStream stream = new FileInputStream(EXE_NAME);		
 		List<String> boolList = new ArrayList<String>();
@@ -510,9 +253,7 @@ public abstract class AbstractStatIndexer {
 		int flight_sprite_length;
 		int explosion_sprite_number;
 		int explosion_sprite_length;
-
 	}
-	
 
 }
 
